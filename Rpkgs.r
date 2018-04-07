886630fec80eb5aac5f78362bf2dbb981fd14169
Masatoshi Katabuchi <mattocci27@gmail.com>
2018-04-07 19:59:58 -0400

    feat: non CRAN pkg

--- Rpkgs.r
+++ Rpkgs.r
@@ -1,9 +1,7 @@
 # works with base packages
-
 argv <- commandArgs(trailingOnly = TRUE)
 
 my_lib <- argv[1]
-
 
 # list of installed packages
 lib <- .libPaths()[1]
@@ -14,9 +12,15 @@ pkg_list <- tmp$Package
 
 res <- sum(grepl(my_lib, pkg_list))
 
-pkgInfoRepo <- function(pkg.name, url = "https://cran.revolutionanalytics.com/") {
-  d = available.packages(contriburl = contrib.url(url))[pkg.name,]
-  as.character(d["Version"])
+av_pk <- available.packages(contriburl = contrib.url("https://cran.revolutionanalytics.com/"))
+
+pkgInfoRepo <- function(pkg.name, av_pk) {
+  if (pkg.name %in% av_pk[,1]){
+    d = av_pk[pkg.name,]
+    as.character(d["Version"])
+  } else {
+    print("not in CRAN")
+  }
 }
 
 if (!is.na(my_lib)) {
@@ -33,11 +37,14 @@ if (!is.na(my_lib)) {
     pkg <- tmp[i, "Package"]
     ver <- tmp[i, "Version"]
 
-    if (pkgInfoRepo(pkg) == ver) {
-      print(paste(pkg, "is up to date"))
-    } else {
-      install.packages(pkg,
-                   repos = "https://cran.revolutionanalytics.com/")
-    }
+    if (str_detect(tmp[i,1],pkg)) {
+      if (pkgInfoRepo(pkg, av_pk) == ver) {
+        print(paste(pkg, "is up to date"))
+      } else {
+        install.packages(pkg,
+                     repos = "https://cran.revolutionanalytics.com/")
+      }
+    } else {print("not in CRAN")}
   }
 }
+
