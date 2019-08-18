0c548f67c85685e1d36acffd6b062bebda7dc8db
mattocci <mattocci27@gmail.com>
2019-08-18 21:19:01 +0800

    i3: border, battery, vol

--- .config/i3/monitor_detect.sh
+++ .config/i3/monitor_detect.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 #st_prev=$(cat /sys/class/drm/card0-HDMI-A-1/status)
+
+
 st_prev="disconnected"
 
 while :
@@ -10,7 +12,9 @@ do
         st_prev=$st_next
         if [ $st_next = "connected" ]; then
           xrandr --output DP1 --primary --scale 1x1 --mode 3440x1440 --right-of eDP1 --panning 3440x1440+2560+0 \
-      --output eDP1 --scale 0.66x0.66 --panning 2560x1440+0+0
+            --output eDP1 --scale 0.66x0.66 --panning 2560x1440+0+0
+          xrandr --dpi 128
+          xset r rate 300 50
         else
             xrandr --output eDP1 --mode 2560x1440 --output HDMI1 --off
         fi
