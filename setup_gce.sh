744fdaf23e9c86987981d17a3ae7ee79eb8b3bad
mattocci <mattocci27@gmail.com>
2020-02-27 13:50:13 +0800

    nvim plug

--- setup_gce.sh
+++ setup_gce.sh
@@ -42,6 +42,7 @@ link_files
 # vim-plug
 curl -fLo ${HOME}/.local/share/nvim/site/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim +PlugInstall +qall
 
 ## zsh-plug manager
 echo "Installing zsh-plug..."
