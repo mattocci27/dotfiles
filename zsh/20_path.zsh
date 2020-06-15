15d6da5ef5f7a700d2253f379ceb4ecb107b9866
Masatoshi Katabuchi <mattocci27@gmail.com>
2020-06-16 00:40:31 +0800

    go path

--- zsh/20_path.zsh
+++ zsh/20_path.zsh
@@ -5,6 +5,7 @@ case `uname` in
   export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
   export PATH=/usr/lib/python3.7/site-packages:$PATH
   export PATH=$PATH:/Users/mattocci/mutagen_darwin_amd64_v0.11.4
+  export PATH=$PATH:/opt/yarn-v1.22.4/bin
  # export PYENV_ROOT="${HOME}/.pyenv"
  # export PATH="${PYENV_ROOT}/bin:$PATH"
  # eval "$(pyenv init -)"
@@ -13,13 +14,14 @@ case `uname` in
  # export R_LIBS_USER R_LIBS=Testing_Tmux
  # export PATH=/Library/TeX/Root/bin/x86_64-darwin:$PATH
  # export PATH="$HOME/.cargo/bin:$PATH"
- 
+  export DISPLAY=localhost:0.0
  # #go
  # export GOPATH=$HOME
  # export PATH=$PATH:$GOPATH/bin
     ;;
   Linux)
   export PATH=/home/mattocci/.cargo/bin:$PATH
+  export PATH=$PATH:/usr/local/go/bin:${PATH}:${HOME}/go/bin
     ;;
 esac
 
