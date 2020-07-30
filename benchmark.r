2b696d97ef3c7373de67e2d948d0941f73c05812
mattocci <mattocci27@gmail.com>
2020-07-30 11:27:54 +0800

    clean

--- benchmark.r
+++
@@ -1,4 +0,0 @@
-sessionInfo()
-system.time(m <- rnorm(10^8))
-m2 <- matrix(m, nrow = 10^4)
-system.time(m2 %*% m2)
--- magicktrackpad.sh
+++
@@ -1,5 +0,0 @@
-cd $HOME/Linux-Magic-Trackpad-2-Driver/linux/drivers/hid
-make
-sudo rmmod hid_magicmouse
-sudo insmod ./hid-magicmouse.ko
-cd
