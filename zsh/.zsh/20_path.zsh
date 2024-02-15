1400ccdc372116fc409f3b22c678ca87d5363bf5
Masatoshi Katabuchi <mattocci27@gmail.com>
2024-02-15 10:29:38 +0000

    update path

--- zsh/.zsh/20_path.zsh
+++ zsh/.zsh/20_path.zsh
@@ -1,7 +1,6 @@
 # Base PATH modifications
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
-export PATH="/usr/local/go/bin:$PATH"
 export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
 export PATH="$HOME/.pyenv/bin:$PATH"
 export PATH="$HOME/.dotnet/tools:$PATH"
@@ -9,15 +8,8 @@ export PATH="$HOME/.dotnet/tools:$PATH"
 # Conditional configurations for Darwin (macOS)
 if [[ "$(uname)" == "Darwin" ]]; then
     export PATH="/usr/local/sbin:$PATH"
-    export PATH="/usr/local/bin:$PATH"
-    export PATH="/Developer/usr/bin:$PATH"
-    export PATH="/Developer/usr/sbin:$PATH"
-    export PATH="/usr/bin:$PATH"
-    export PATH="/bin:$PATH"
-    export PATH="/usr/sbin:$PATH"
-    export PATH="/sbin:$PATH"
+    export PATH="/usr/local/bin:$PATH" # Likely unnecessary but included for custom installations
     export PATH="/usr/local/opt:$PATH"
-    export PATH="/usr/lib:$PATH"
     export PATH="/usr/local/opt/avr-gcc@8/bin:$PATH"
     export PATH="/opt/yarn-v1.22.4/bin:$PATH"
     export PATH="/Applications/Julia-1.7.app/Contents/Resources/julia/bin:$PATH"
@@ -25,12 +17,13 @@ if [[ "$(uname)" == "Darwin" ]]; then
     export PATH="$HOME/bin/context/tex/texmf-osx-arm64/bin:$PATH"
     export PATH="$HOME/Library/TinyTeX/bin/universal-darwin:$PATH"
     export PATH="$HOME/context/tex/texmf-osx-arm64/bin:$PATH"
-    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
     export PATH="$HOME/Dropbox/src/github.com/ranger/ranger:$PATH"
+    export RUBY_CONFIGURE_OPTS="--with-openssl-dir=$(brew --prefix openssl@1.1)"
 elif [[ "$(uname)" == "Linux" ]]; then
     export PATH="$HOME/.config/i3:$PATH"
-    export PATH="$HOME/.local/bin:$PATH"
-    export PATH="$HOME/quarto-cli/package/dist/bin/quarto$PATH"
+    export PATH="/usr/local/go/bin:$PATH"
+    export PATH="$HOME/go/bin:$PATH"
+    export PATH="$HOME/quarto-cli/package/dist/bin/quarto:$PATH"
     if [[ "$(lsb_release -si)" == "microsoft" ]]; then
         # Configuration for WSL2
         export DISPLAY="$(awk '/nameserver/ {print $2}' /etc/resolv.conf):0"
