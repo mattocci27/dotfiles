6064385ee4833e66feca83ac62f78ca0501d5f23
Masatoshi Katabuchi <mattocci27@gmail.com>
2024-02-28 22:15:08 +0800

    Add initial configuration settings for Neovim in VS Cod

--- nvim/init_vscode.lua
+++ nvim/init_vscode.lua
@@ -54,7 +54,16 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  "tpope/vim-surround",
   {"xiyaowong/fast-cursor-move.nvim"}, -- Corrected syntax
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+  },
 })
 
