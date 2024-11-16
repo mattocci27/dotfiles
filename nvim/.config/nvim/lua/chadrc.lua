1574995386e032b2f048f2f4c2198db7b22f1642
Masatoshi Katabuchi <mattocci27@gmail.com>
2024-11-16 12:02:44 +0800

    use NvUI v3.0

--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -4,15 +4,29 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
-  theme = "gruvbox",
-  theme_toggle = { "gruvbox", "one_light" },
+M.base46 = {
+  theme = "aquarium",
+  -- theme_toggle = { "aquarium", "one_light" },
   -- hl_override = highlights.override,
-  -- hl_add = highlights.add,
-  transparency = true,
-  nvdash = {
-    load_on_startup = true,
+  -- hl_add = require"hl",
+  transparency = true
+}
+
+-- UI settings
+M.ui = {
+  statusline = {
+    theme = "default",     -- Statusline theme: default, minimal, etc.
+    separator_style = "round", -- Separator style: default, arrow, block, etc.
+  },
+  tabufline = {
+    enabled = true,        -- Enable the tabufline
+    lazyload = true,       -- Lazy load when there are multiple buffers
   },
 }
+
+M.nvdash = {
+  load_on_startup = true,
+}
+
 
 return M
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,13 +1,12 @@
 return {
   {
     "stevearc/conform.nvim",
-    -- event = 'BufWritePre', -- uncomment for format on save
+    -- Uncomment to enable format on save
+    -- event = 'BufWritePre',
     config = function()
       require "configs.conform"
     end,
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -15,48 +14,48 @@ return {
       require "configs.lspconfig"
     end,
   },
-
   {
-  	"williamboman/mason.nvim",
-  	opts = {
-  		ensure_installed = {
-  			"lua-language-server", "stylua",
-  			"html-lsp", "css-lsp" , "prettier"
-  		},
-  	},
-  },
-
-  {
-  	"nvim-treesitter/nvim-treesitter",
-  	opts = {
-  		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css"
-  		},
-  	},
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+      },
+    },
   },
   {
-  "ojroques/nvim-osc52",
-  keys = {
-    {
-      "<leader>y",
-      function() return require("osc52").copy_operator() end, -- <-- previously I wasn't using a return statement here
-      desc = "copy selection to system clipboard (normal mode)",
-      expr = true
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc", "html", "css",
+      },
     },
-    {
-      "<leader>Y",
-      "<leader>y_",
-      remap = true,
-      desc = "copy current line into system clipboard (normal mode)"
+  },
+  {
+    "ojroques/nvim-osc52",
+    keys = {
+      {
+        "<leader>y",
+        function() require("osc52").copy_operator() end,
+        desc = "Copy selection to system clipboard (normal mode)",
+        expr = true,
+      },
+      {
+        "<leader>Y",
+        "<leader>y_",
+        remap = true,
+        desc = "Copy current line into system clipboard (normal mode)",
+      },
+      {
+        mode = "v",
+        "<leader>y",
+        function() require("osc52").copy_visual() end,
+        desc = "Copy selection to system clipboard (visual mode)",
+      },
     },
-    {
-      mode = "v",
-      "<leader>y",
-      function() require("osc52").copy_visual() end,
-      desc = "copy selection to system clipboard (visual mode)"
-    }
-   }
   },
   {
     "christoomey/vim-tmux-navigator",
@@ -68,22 +67,20 @@ return {
       "TmuxNavigatePrevious",
     },
     keys = {
-      { "<C-h>", "<cmd> TmuxNavigateLeft <CR>" },
-      { "<C-j>", "<cmd> TmuxNavigateDown <CR>" },
-      { "<C-k>", "<cmd> TmuxNavigateUp <CR>" },
-      { "<C-l>", "<cmd> TmuxNavigateRight <CR>" },
-      { "<C-\\>", "<cmd> TmuxNavigatePrevious <CR>" },
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Navigate left (Tmux)" },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Navigate down (Tmux)" },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Navigate up (Tmux)" },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Navigate right (Tmux)" },
+      { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", desc = "Navigate to previous (Tmux)" },
     },
   },
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
+      require("nvim-surround").setup()
+    end,
   },
   {
     "iamcco/markdown-preview.nvim",
@@ -96,41 +93,56 @@ return {
   {
     "shortcuts/no-neck-pain.nvim",
     cmd = "NoNeckPain",
-    keys = { { "<leader>nn", "<cmd>NoNeckPain<cr>", desc = "[N]o [N]eckpain" } },
+    keys = { { "<leader>nn", "<cmd>NoNeckPain<CR>", desc = "No Neck Pain Mode" } },
     opts = {},
   },
   {
-    'quarto-dev/quarto-nvim',
-    ft = { 'quarto' },
-    dev = false,
+    "quarto-dev/quarto-nvim",
+    ft = { "quarto" },
     opts = {
       lspFeatures = {
-        languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html', 'dot', 'javascript', 'typescript', 'ojs' },
+        languages = { "r", "python", "julia", "bash", "lua", "html", "dot", "javascript", "typescript", "ojs" },
       },
       codeRunner = {
         enabled = true,
-        default_method = 'slime',
+        default_method = "slime",
       },
     },
     dependencies = {
       {
-        'jmbuhr/otter.nvim',
-        dev = false,
-        dependencies = {
-          { 'neovim/nvim-lspconfig' },
-        },
+        "jmbuhr/otter.nvim",
+        dependencies = { "neovim/nvim-lspconfig" },
         opts = {
           lsp = {
-            hover = {
-              border = border_style,
-            },
+            hover = { border = "rounded" }, -- Define `border_style` if needed
           },
-          buffers = {
-            set_filetype = true,
-          },
+          buffers = { set_filetype = true },
           handle_leading_whitespace = true,
         },
       },
     },
+  },
+  {
+    "nvim-lua/plenary.nvim",
+  },
+  {
+    "nvchad/ui",
+    config = function()
+      require "nvchad"
+    end,
+  },
+  {
+    "nvchad/base46",
+    lazy = true,
+    build = function()
+      require("base46").load_all_highlights()
+    end,
+  },
+  {
+    "nvchad/volt",
+    lazy = true,
+  },
+  {
+    "williamboman/mason.nvim",
   },
 }
