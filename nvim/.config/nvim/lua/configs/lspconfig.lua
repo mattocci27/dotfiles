3a444b5f400ba4374a1d21e23489bdfa22e911bd
Masatoshi Katabuchi <mattocci27@gmail.com>
2026-02-25 10:41:20 +0800

    update nvim

--- nvim/.config/nvim/lua/configs/lspconfig.lua
+++ nvim/.config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,28 @@
--- load defaults i.e lua_lsp
+-- -- Load NvChad defaults (like lua_ls)
 require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require "lspconfig"
-
--- EXAMPLE
-local servers = { "html", "cssls" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config
+-- List your servers here
+local servers = { "html", "cssls" }
+
+-- Loop through and initialize servers using the new 0.11+ syntax
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  vim.lsp.config(lsp, {
     on_attach = nvlsp.on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
-  }
+  })
+  
+  -- This tells Neovim to actually start the server for its filetypes
+  vim.lsp.enable(lsp)
 end
 
--- configuring single server, example: typescript
--- lspconfig.ts_ls.setup {
+-- Example: Configuring a single server with specific settings (like ts_ls)
+-- vim.lsp.config("ts_ls", {
 --   on_attach = nvlsp.on_attach,
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
--- }
+--   -- settings = { ... } -- custom settings go here
+-- })
+-- vim.lsp.enable("ts_ls")
